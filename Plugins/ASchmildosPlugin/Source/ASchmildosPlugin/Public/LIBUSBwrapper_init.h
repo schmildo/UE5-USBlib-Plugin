71d77e9af2a2f0682596dc4335a60ad2ef51ba8d
schmildo <37068749+schmildo@users.noreply.github.com>
2023-03-01 21:57:47 +1100

    Did nothing

---
+++ Plugins/ASchmildosPlugin/Source/ASchmildosPlugin/Public/LIBUSBwrapper_init.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "LIBUSBwrapper_init.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASCHMILDOSPLUGIN_API ULIBUSBwrapper_init : public UObject
+{
+	GENERATED_BODY()
+	
+};
